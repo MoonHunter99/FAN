73b3142b4910786bbb722c1882109ccfc3328571
Neil Daniel G. Vergara <vergaraneildaniel@gmail.com>
2023-07-04 21:17:12 +0800

    constructor complete hehehhehe nakalimutan mag-save

---
+++ Fan_Class.py
@@ -0,0 +1,6 @@
+class FAN:
+    def __init__(self, speed = 1, power = False, radius = 5, color = 'Blue'):
+        self.speed = speed
+        self.power = power
+        self.radius = radius
+        self.color = color
