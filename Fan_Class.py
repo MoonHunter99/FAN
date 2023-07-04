a4e7398c534ea43a709b5565e6f193de042578a8
Neil Daniel G. Vergara <vergaraneildaniel@gmail.com>
2023-07-04 21:31:29 +0800

    get speed completed

--- Fan_Class.py
+++ Fan_Class.py
@@ -1,6 +1,13 @@
 class FAN:
     def __init__(self, speed = 1, power = False, radius = 5, color = 'Blue'):
-        self.speed = speed
-        self.power = power
-        self.radius = radius
-        self.color = color
+        self.__speed = speed
+        self.__power = power
+        self.__radius = radius
+        self.__color = color
+    def get_speed(self):
+        if self.__speed == 1:
+            print("SLOW")
+        elif self.__speed == 2:
+            print("MEDIUM")
+        elif self.__speed == 3:
+            print("FAST")
